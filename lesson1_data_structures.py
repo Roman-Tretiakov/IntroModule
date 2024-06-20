735cb3f64630277b8a2119d777e77e4d9ad5bdfd
Roman_Tretiakov <romantre@wizardsdev.com>
2024-06-20 21:53:01 +0300

    first commit

---
+++ lesson1_data_structures.py
@@ -0,0 +1,38 @@
+#Word length:
+a = 'Word_length'
+length = len(a)
+
+print(length)
+
+#Sums and differences:
+first = 10
+second = 15
+
+sum = first + second
+diff = first - second
+
+print(sum, diff)
+
+#Average:
+first = 10
+second = 15
+third = 2
+
+mean = (first + second + third) / 3
+
+print(mean)
+
+#Simple strings:
+first_string = 'Вторник'
+second_string = 'Понедельник'
+
+print(second_string + ", " + first_string)
+
+#Complex formula:
+a = 10
+b = -5
+c = 2
+
+f = ((a * b) + (a * c)) ** 3
+
+print(f / 2)
